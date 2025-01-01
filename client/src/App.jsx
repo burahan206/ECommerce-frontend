0acd3da332a6cb59c324586517b655de7deeba2e
burahan206 <141344974+burahan206@users.noreply.github.com>
2025-01-01 14:50:00 +0700

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 class="bg-red-500 text-white p-4" >Vite + React</h1>
+      <h1 >Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
